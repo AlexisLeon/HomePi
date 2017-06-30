f04bda2f52b0fbb57e85262e88f27ab75920311d
Alexis Leon <alexis.leon@icloud.com>
2017-06-29 22:29:18 -0500

    License, more logger params and address vars

--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,38 @@
+/**
+ * Copyright (c) 2017 Alexis Leon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /* eslint-disable no-console */
 const chalk = require('chalk');
 
-module.exports = (color, content) => console.log(chalk[color](content));
+// Log data with up to 3 different colors
+module.exports = (color, content, color1, content1, color2, content2) => {
+  if (color1 && content1) {
+    if (color2 && content2) {
+      console.log(chalk[color](content), chalk[color1](content1), chalk[color2](content2));
+    }
+
+    console.log(chalk[color](content), chalk[color1](content1));
+  } else {
+    console.log(chalk[color](content));
+  }
+};
 module.exports.colors = chalk;
